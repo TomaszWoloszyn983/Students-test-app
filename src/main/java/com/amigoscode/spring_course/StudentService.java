610be3a913db6b051c2f246d21242b58bc053c34
Tomasz Woloszyn <tomaszwoloszyn983@gmail.com>
2023-03-20 07:24:14 +0000

    Add StudentRepository interface. Create getAllStudents function.

--- src/main/java/com/amigoscode/spring_course/StudentService.java
+++ src/main/java/com/amigoscode/spring_course/StudentService.java
@@ -9,15 +9,13 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    public StudentService (StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Zdzislaw",
-                        "zdzichu@dupa.com",
-                        LocalDate.of(1979, Month.JANUARY, 13),
-                        44
-                )
-        );
+        return studentRepository.findAll();
     }
 }
