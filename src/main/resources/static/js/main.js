759b3dff06f6b1b1c8dcda0549c0ea7fd5da2f1e
Tomasz Woloszyn <tomaszwoloszyn983@gmail.com>
2023-06-11 09:54:34 +0100

    Add messages timeout function.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,5 +2,11 @@
  * 
  */
 
-
+// Messages Timeout function Doesn't work from here becauce the JavaScript fila doesn't load.
+let warMessages = document.getElementById("warningMessage");
+if (warMessages != null){
+        setTimeout(function(){
+        warMessages.style.display = "none";
+    }, 3000);
+}
 
